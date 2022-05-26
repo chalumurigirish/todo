dd5f45ce93c15b94dac450ca2e01e9c7da32756a
chalumurigirish <girishchalumuri@gmail.com>
2022-05-26 15:05:45 +0530

    chore:debug 7

--- pages/api/register.js
+++ pages/api/register.js
@@ -1,4 +1,34 @@
-import admin from '@/utils/firebaseAdmin';
+// import admin from '@/utils/firebaseAdmin';
+
+import * as admin from 'firebase-admin';
+
+const firebaseAdminConfig = {
+  type: process.env.FB_TYPE,
+  project_id: process.env.FB_PROJECT_ID,
+  private_key_id: process.env.FB_PRIVATE_KEY_ID,
+  private_key: process.env.FB_PRIVATE_KEY,
+  client_email: process.env.FB_CLIENT_EMAIL,
+  client_id: process.env.FB_CLIENT_ID,
+  auth_uri: process.env.FB_AUTH_URI,
+  token_uri: process.env.FB_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FB_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FB_CLIENT_CERT_URL,
+};
+
+(async () => {
+  try {
+    await admin.initializeApp({
+      credential: admin.credential.cert(firebaseAdminConfig),
+    });
+    console.log('initialization');
+  } catch (error) {
+    if (!/already exists/u.test(error.message)) {
+      console.error('Firebase admin initialization error', error.stack);
+    }
+  }
+})();
+
+// export default admin;
 
 const register = async (req, res) => {
   const { email, password, displayName } = req.body.input.credentials;
@@ -15,7 +45,6 @@ const register = async (req, res) => {
         'x-hasura-allowed-roles': ['user'],
         'x-hasura-default-role': 'user',
         'x-hasura-user-id': user.uid,
-        'x-hasura-id': user.uid,
       },
     });
 
