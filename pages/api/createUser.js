e6eeaf1052d2c3375fdb470ecd6e563b2d22015f
chalumurigirish <girishchalumuri@gmail.com>
2022-05-26 12:45:08 +0530

    chore:debug 1

--- pages/api/createUser.js
+++ pages/api/createUser.js
@@ -16,43 +16,48 @@ const firebaseAdminConfig = {
 
 const createUser = async (req, res) => {
   const { email, password, displayName } = req.body.input.credentials;
+  res.status(200).json({
+    id: 'girish',
+    email,
+    displayName,
+  });
 
-  if (admin?.apps?.length === 0) {
-    console.log('initializeApp');
-    const app = await admin.initializeApp({
-      credential: admin.credential.cert(firebaseAdminConfig),
-    });
-  } else {
-    console.log('already initialized');
-  }
+  //   if (admin?.apps?.length === 0) {
+  //     console.log('initializeApp');
+  //     const app = await admin.initializeApp({
+  //       credential: admin.credential.cert(firebaseAdminConfig),
+  //     });
+  //   } else {
+  //     console.log('already initialized');
+  //   }
 
-  try {
-    const user = await admin.auth().createUser({
-      email: email,
-      password: password,
-      displayName: displayName,
-    });
+  //   try {
+  //     const user = await admin.auth().createUser({
+  //       email: email,
+  //       password: password,
+  //       displayName: displayName,
+  //     });
 
-    await admin.auth().setCustomUserClaims(user.uid, {
-      'https://hasura.io/jwt/claims': {
-        'x-hasura-allowed-roles': ['user'],
-        'x-hasura-default-role': 'user',
-        'x-hasura-user-id': user.uid,
-        'x-hasura-id': user.uid,
-      },
-    });
+  //     await admin.auth().setCustomUserClaims(user.uid, {
+  //       'https://hasura.io/jwt/claims': {
+  //         'x-hasura-allowed-roles': ['user'],
+  //         'x-hasura-default-role': 'user',
+  //         'x-hasura-user-id': user.uid,
+  //         'x-hasura-id': user.uid,
+  //       },
+  //     });
 
-    return res.status(200).json({
-      id: user.uid,
-      email: user.email,
-      displayName: user.displayName,
-    });
-  } catch (error) {
-    return res.status(501).json({
-      error: error.message,
-      location: '/api/createUser was failing',
-    });
-  }
+  //     return res.status(200).json({
+  //       id: user.uid,
+  //       email: user.email,
+  //       displayName: user.displayName,
+  //     });
+  //   } catch (error) {
+  //     return res.status(501).json({
+  //       error: error.message,
+  //       location: '/api/createUser was failing',
+  //     });
+  //   }
 };
 
 export default createUser;
