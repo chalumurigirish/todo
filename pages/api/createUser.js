21303a187f124b5dce8c7c2aa2f78925431ee1c2
chalumurigirish <girishchalumuri@gmail.com>
2022-05-26 11:48:50 +0530

    chore: fix 2

--- pages/api/createUser.js
+++ pages/api/createUser.js
@@ -1,4 +1,26 @@
-import admin from '@/utils/firebaseAdmin';
+import * as admin from 'firebase-admin';
+
+const firebaseAdminConfig = {
+  type: process.env.FB_TYPE,
+  project_id: process.env.FB_PROJECT_ID,
+  private_key_id: process.env.FB_PRIVATE_KEY_ID,
+  private_key: process.env.FB_PRIVATE_KEY,
+  client_email: process.env.FB_CLIENT_EMAIL,
+  client_id: process.env.FB_CLIENT_ID,
+  auth_uri: process.env.FB_AUTH_URI,
+  token_uri: process.env.FB_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FB_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FB_CLIENT_CERT_URL,
+};
+
+if (admin.apps.length === 0) {
+  console.log('initializeApp');
+  admin.initializeApp({
+    credential: admin.credential.cert(firebaseAdminConfig),
+  });
+} else {
+  console.log('already initialized');
+}
 
 const createUser = async (req, res) => {
   const { email, password, displayName } = req.body.input.credentials;
